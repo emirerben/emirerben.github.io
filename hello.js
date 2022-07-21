37b0092646c168729d314f51ff564fd1cfbbae98
erben <erben@umich.edu>
2022-07-21 08:34:05 -0400

    horizontal slide not great

---
+++ hello.js
@@ -0,0 +1,8 @@
+console.log('Hello, World!');
+
+function scrolll(){
+    var left = document.querySelector(".container");
+    left.scrollBy(-500,0);
+}
+
+let button = document.getElementById(vamos)
